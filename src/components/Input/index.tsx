9756e517a774d1ba5814546aef0c1df7909faab4
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-21 11:17:38 -0500

    fixed errors lint

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,15 @@
-import React, { InputHTMLAttributes, useState } from 'react'
+import React, { FC, InputHTMLAttributes } from 'react'
 import styles from './styles.module.css'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   labelValue: string
 }
 
-function Input({ labelValue, ...props }: Props) {
+const Input: FC<Props> = ({ labelValue, ...props }) => {
   return (
     <div className={styles.wrapper}>
       <label className={styles.label}>{labelValue}</label>
-      <input className={styles.element} spellCheck="false" {...props} />
+      <input className={styles.element} spellCheck='false' {...props} />
     </div>
   )
 }
