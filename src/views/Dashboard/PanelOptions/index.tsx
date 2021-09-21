38515ad1f16eeb77860849e229516f8da81addf3
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-21 18:09:37 -0500

    add panel options for dashboard

---
+++ src/views/Dashboard/PanelOptions/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import styles from './styles.module.css'
+
+const PanelOptions: FC<{}> = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h2>Opciones</h2>
+    </section>
+  )
+}
+
+export default PanelOptions
