7c1594bad0ca3a0d03e7510e2e444db510605121
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-21 10:21:00 -0500

    import files with styles css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import './index.css'
+import './styles/index.css'
+import './styles/global.css'
 
 ReactDOM.render(<App />,  document.getElementById('root'))
