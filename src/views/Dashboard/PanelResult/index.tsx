3ae0e39750b05d07f5323f857ed71dc15f55a85b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-21 18:10:04 -0500

    add panel result for dashboard

---
+++ src/views/Dashboard/PanelResult/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import styles from './styles.module.css'
+
+const PanelResult: FC<{}> = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h2>Resultado</h2>
+    </section>
+  )
+}
+
+export default PanelResult
