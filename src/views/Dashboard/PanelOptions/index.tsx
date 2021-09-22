99b4eff34609ae441222f4af1bac666fd38e4abf
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-21 23:18:35 -0500

    add inputColor to options

--- src/views/Dashboard/PanelOptions/index.tsx
+++ src/views/Dashboard/PanelOptions/index.tsx
@@ -1,15 +1,17 @@
 import React, { FC } from 'react'
 import InputColor from '../../../components/InputColor'
+import useField from '../../../hooks/useField'
 import styles from './styles.module.css'
 
 const PanelOptions: FC<{}> = () => {
+  const color = useField('#ccc')
   return (
     <section className={styles.wrapper}>
       <header className={styles.header}>
         <h4 className={styles.header__title}>Opciones</h4>
       </header>
       <div className={styles.panel__wrapper}>
-        <InputColor labelValue='Variables Iniciales' />
+        <InputColor labelValue='Variables Iniciales' {...color} />
       </div>
     </section>
   )
