e3f735be82a29f79116fb34569e8757ade031a0f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-25 15:56:25 -0500

    add component OptionsInput

--- src/views/Dashboard/PanelOptions/index.tsx
+++ src/views/Dashboard/PanelOptions/index.tsx
@@ -1,25 +1,15 @@
 import React, { FC } from 'react'
-import GroupInputs from '../../../components/GroupInputs'
-import Input from '../../../components/Input'
-import InputColor from '../../../components/InputColor'
-import useField from '../../../hooks/useField'
+import OptionsInput from './OptionsInput'
 import styles from './styles.module.css'
 
 const PanelOptions: FC<{}> = () => {
-  const color = useField('color', '#fafafa')
-  const bgColor = useField('color', '#04aa6d')
-
   return (
     <section className={styles.wrapper}>
       <header className={styles.header}>
         <h4 className={styles.header__title}>Opciones</h4>
       </header>
       <div className={styles.panel__wrapper}>
-        <GroupInputs labelValue='Variables Iniciales'>
-          <Input type='number' />
-          <InputColor {...bgColor} />
-          <InputColor {...color} />
-        </GroupInputs>
+        <OptionsInput labelValue='Entradas Iniciales' name='styleInitialVariables' />
       </div>
     </section>
   )
