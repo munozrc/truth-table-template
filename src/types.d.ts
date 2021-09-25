76dd24607fff65a4ec53b5d584d92e5ccb08b62a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-25 14:48:34 -0500

    add type Table

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+export interface TableSettings {
+  styleInitialVariables: styleProperties
+  styleOneValue: styleProperties
+  styleZeroValue: styleProperties
+  zoom: number
+}
+
+export interface styleProperties {
+  color: string
+  bgColor: string
+}
