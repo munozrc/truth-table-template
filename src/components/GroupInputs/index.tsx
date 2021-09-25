bc14442f05392421b7b887219bffb40601f09481
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-25 15:13:43 -0500

    create component to grouping inputs

---
+++ src/components/GroupInputs/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import styles from './styles.module.css'
+
+interface Props {
+  labelValue?: string
+}
+
+const GroupInputs: FC<Props> = ({ labelValue = '', children }) => {
+  return (
+    <section className={styles.wrapper}>
+      <label className={styles.label}>{labelValue}</label>
+      <div className={styles.inputs}>
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export default GroupInputs
--- src/views/Dashboard/PanelOptions/index.tsx
+++ src/views/Dashboard/PanelOptions/index.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from 'react'
+import GroupInputs from '../../../components/GroupInputs'
 import Input from '../../../components/Input'
 import InputColor from '../../../components/InputColor'
 import useField from '../../../hooks/useField'
 import styles from './styles.module.css'
 
 const PanelOptions: FC<{}> = () => {
-  const color = useField('color', '#04AA6D')
+  const color = useField('color', '#fafafa')
+  const bgColor = useField('color', '#04aa6d')
   return (
     <section className={styles.wrapper}>
       <header className={styles.header}>
         <h4 className={styles.header__title}>Opciones</h4>
       </header>
       <div className={styles.panel__wrapper}>
-        <Input labelValue='Variables Iniciales' />
-        <InputColor labelValue='Background' {...color} />
-        <InputColor labelValue='Foreground' {...color} />
+        <GroupInputs labelValue='Variables Iniciales'>
+          <Input />
+          <InputColor {...bgColor} />
+          <InputColor {...color} />
+        </GroupInputs>
       </div>
     </section>
   )
