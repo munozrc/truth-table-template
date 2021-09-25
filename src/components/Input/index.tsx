5f58f1b5dc56dd5feda4ba141a3cb9a5395b8eb0
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-25 15:12:48 -0500

    set labelValue with not requiered

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,13 +2,13 @@ import React, { FC, InputHTMLAttributes } from 'react'
 import styles from './styles.module.css'
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  labelValue: string
+  labelValue?: string
 }
 
-const Input: FC<Props> = ({ labelValue, ...props }) => {
+const Input: FC<Props> = ({ labelValue = '', ...props }) => {
   return (
     <div className={styles.wrapper}>
-      <label className={styles.label}>{labelValue}</label>
+      {labelValue !== '' && <label className={styles.label}>{labelValue}</label>}
       <input className={styles.element} spellCheck='false' {...props} />
     </div>
   )
