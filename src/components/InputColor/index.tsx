451d0ded9fdee24b00f1d79f2054a2879a19b3c9
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-25 15:12:00 -0500

    refactor inputColor

--- src/components/InputColor/index.tsx
+++ src/components/InputColor/index.tsx
@@ -1,17 +1,10 @@
 import React, { FC, InputHTMLAttributes } from 'react'
 import styles from './styles.module.css'
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  labelValue: string
-}
-
-const InputColor: FC<Props> = ({ labelValue, ...props }) => {
+const InputColor: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.wrapper__input}>
-        <input className={styles.element} {...props} type='color' />
-        <label className={styles.label}>{labelValue}</label>
-      </div>
+      <input className={styles.element} {...props} type='color' />
     </div>
   )
 }
