041f6a36d858c79b9e0da13b3eee3c118410559b
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-24 21:12:24 -0500

    add toolbar

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import { FiClipboard, FiInfo, FiTrash2 } from 'react-icons/fi'
+import Button from '../../components/Button'
 import PanelOptions from './PanelOptions'
 import PanelResult from './PanelResult'
 import styles from './styles.module.css'
@@ -8,6 +10,11 @@ const Dashboard: FC<{}> = () => {
     <div className={styles.wrapper}>
       <header className={styles.wrapper__header}>
         <h1 className={styles.header__title}>😭Template</h1>
+        <div className={styles.wrapper__toolbar}>
+          <Button variant='flat'><FiClipboard size='1.4em' color='#fafafa' /></Button>
+          <Button variant='flat'><FiTrash2 size='1.4em' color='#fafafa' /></Button>
+          <Button variant='flat'><FiInfo size='1.4em' color='#fafafa' /></Button>
+        </div>
       </header>
       <PanelResult />
       <PanelOptions />
