1421d38c6460773865848a145f11fb8fcee27ddd
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-27 18:39:37 -0500

    add parseBinary and generateArrayNumbers

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export const parseBinary = (value: number, lenght: number): string[] => {
+  const decimalToBinary = value.toString(2).padStart(lenght, '0')
+  const getDigits = decimalToBinary.split('')
+  return getDigits
+}
+
+export const generateArrayNumbers = (maxValue: number): number[] => {
+  return Array.from(Array(Math.pow(2, maxValue)).keys())
+}
