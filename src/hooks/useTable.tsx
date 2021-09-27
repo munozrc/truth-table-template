fc57d5af4d030133e658e2ad2ec3718cff5f9249
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-27 16:39:57 -0500

    change ouput hook useTable

--- src/hooks/useTable.tsx
+++ src/hooks/useTable.tsx
@@ -1,15 +1,17 @@
 import { useCallback, useContext } from 'react'
 import TableContext from '../contexts/TableContext'
-import { styleProperties } from '../types'
+import { styleProperties, TableData } from '../types'
 
 interface ReturnType {
-  styleInitialVariables: styleProperties
+  stylesEntries: styleProperties
+  stylesOutputs: styleProperties
+  tableData: TableData
   changeStyles: (element: string, values: styleProperties) => void
 }
 
 function useTable (): ReturnType {
-  const { tableSettings, changeTableSettings } = useContext(TableContext)
-  const { styleInitialVariables } = tableSettings
+  const { tableSettings, tableData, changeTableSettings } = useContext(TableContext)
+  const { stylesEntries, stylesOutputs } = tableSettings
 
   const changeStyles = useCallback((element: string, values: styleProperties) => {
     changeTableSettings(prev => {
@@ -18,7 +20,7 @@ function useTable (): ReturnType {
     })
   }, [])
 
-  return { styleInitialVariables, changeStyles }
+  return { stylesEntries, stylesOutputs, tableData, changeStyles }
 }
 
 export default useTable
