aff2680f51d417d519ce621d09fd7ae6bf82f463
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-27 17:14:52 -0500

    test generate table for data

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -9,34 +9,45 @@ export interface PropsTable {
 }
 
 const Table = forwardRef<HTMLTableElement, PropsTable>((props, ref) => {
-  const { stylesEntries } = props
+  const { stylesEntries, tableData } = props
   return (
     <table ref={ref} className={styles.table}>
       <tbody>
         <tr>
-          <th
-            className={styles.th}
-            style={{ backgroundColor: stylesEntries.bgColor, color: stylesEntries.color }}
-          >A
-          </th>
-          <th
-            className={styles.th}
-            style={{ backgroundColor: stylesEntries.bgColor, color: stylesEntries.color }}
-          >B
-          </th>
-          <th
-            className={styles.th}
-            style={{ backgroundColor: stylesEntries.bgColor, color: stylesEntries.color }}
-            colSpan={2}
-          >X
-          </th>
+          {
+            tableData.initialEntries.map(entry => (
+              <th
+                key={entry}
+                className={styles.th}
+                style={{ backgroundColor: stylesEntries.bgColor, color: stylesEntries.color }}
+              >{entry}
+              </th>
+            ))
+          }
+          {
+            Object.keys(tableData.outputs).map((output, index) => (
+              <th
+                key={`th-${index}`}
+                className={styles.th}
+                style={{ backgroundColor: stylesEntries.bgColor, color: stylesEntries.color }}
+                colSpan={2}
+              >{output.toUpperCase()}
+              </th>
+            ))
+          }
         </tr>
-        <tr>
-          <td className={styles.td}>0</td>
-          <td className={styles.td}>1</td>
-          <td className={styles.td}>1</td>
-          <td className={styles.td__expression}>A'B</td>
-        </tr>
+        {
+          Object.keys(tableData.outputs).map((output) => (
+            tableData.outputs[output].map((value, index) => (
+              <tr key={`${output}-${index}`}>
+                <td className={styles.td}>0</td>
+                <td className={styles.td}>1</td>
+                <td className={styles.td}>{value}</td>
+                <td className={styles.td__expression} />
+              </tr>
+            ))
+          ))
+        }
       </tbody>
     </table>
   )
