0f92f44891e6ca4a982f95c5dd9bc496f8a74fe7
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-27 18:57:50 -0500

    add bounce function

--- src/views/Dashboard/PanelOptions/OptionsInput.tsx
+++ src/views/Dashboard/PanelOptions/OptionsInput.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useCallback, useEffect } from 'react'
+import debounce from 'lodash.debounce'
 import GroupInputs from '../../../components/GroupInputs'
 import Input from '../../../components/Input'
 import InputColor from '../../../components/InputColor'
@@ -16,9 +17,17 @@ const OptionsInput: FC<Props> = ({ labelValue, name }) => {
   const bgColor = useField('color', '#04aa6d')
   const color = useField('color', '#fafafa')
 
+  const changeHandler = (color: string, bgColor: string): void => {
+    changeStyles(name, { color, bgColor })
+  }
+
+  const debouncedChangeHandler = useCallback(
+    debounce(changeHandler, 300)
+    , [])
+
   useEffect(() => {
-    changeStyles(name, { color: color.value, bgColor: bgColor.value })
-  }, [bgColor.value, color.value])
+    debouncedChangeHandler(color.value, bgColor.value)
+  }, [color.value, bgColor.value])
 
   return (
     <GroupInputs labelValue={labelValue}>
