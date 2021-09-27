b182ec3b2aac645421f8728c27bee5956ef92b26
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-09-27 16:40:18 -0500

    pass new props TableComponent

--- src/views/Dashboard/PanelResult/index.tsx
+++ src/views/Dashboard/PanelResult/index.tsx
@@ -1,15 +1,22 @@
 import React, { FC, RefObject } from 'react'
 import Table from '../../../components/Table'
+import useTable from '../../../hooks/useTable'
 import styles from './styles.module.css'
 
 interface Props {
-  refTable: RefObject<any>
+  refTable: RefObject<HTMLTableElement>
 }
 
 const PanelResult: FC<Props> = ({ refTable }) => {
+  const { stylesEntries, stylesOutputs, tableData } = useTable()
   return (
     <section className={styles.wrapper}>
-      <Table ref={refTable} />
+      <Table
+        ref={refTable}
+        stylesEntries={stylesEntries}
+        stylesOutputs={stylesOutputs}
+        tableData={tableData}
+      />
     </section>
   )
 }
